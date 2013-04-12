1105b6cac5cf87ed6a4ec56eee6aaee8290962c0
daniw <dani.winz@bluewin.ch>
2013-04-13 00:31:03 +0200

    exercise during lesson

---
+++ sw07/massball.m
@@ -0,0 +1,16 @@
+% Calculate the mass of a ball
+
+% Clear wirkspace and command window
+clear; clc; 
+
+% Diamater of the ball
+d=2;
+
+% Density
+rho=9500;
+
+% Volume 
+V=4/3*pi*(d/2)^3;
+
+% Mass
+M=rho*V
