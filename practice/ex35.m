e17a0680d671d35e67694b758b49c23509611009
daniw <dani.winz@bluewin.ch>
2013-04-27 19:54:20 +0200

    Aufgabe 3.5

---
+++ practice/ex35.m
@@ -0,0 +1,30 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+h=25;
+g=9.81;
+m=60;
+tb=0.006;
+
+% a
+
+% Zwischenewerte berechnen
+
+% Berechnung der Ergebnisse
+v=sqrt(2*g*h)
+
+% b
+
+% Zwischenwerte berechnen
+
+% Berechnung der Ergebnisse
+j=m*v
+
+% c
+
+% Zwischenwerte berechnen
+
+% Berechnung der Ergebnisse
+f=j/tb
+
