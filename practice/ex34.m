9904a3b6610df4ed9381e8c25b769937caa7d680
daniw <dani.winz@bluewin.ch>
2013-04-27 19:17:15 +0200

    Aufgabe 3.4 a, b, c provisorisch

---
+++ practice/ex34.m
@@ -0,0 +1,32 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+ms=67.8;
+mb=0.53;
+h=0.44;
+vb=19.6;
+g=9.81;
+alphagrad=30.6;
+
+% a
+
+% Zwischenewerte berechnen
+
+% Berechnung der Ergebnisse
+vsa=0
+
+% b
+
+% Zwischenwerte berechnen
+
+% Berechnung der Ergebnisse
+vsb=mb*vb/ms
+
+% c
+
+% Zwischenwerte berechnen
+alpha=alphagrad/180*pi;
+
+% Berechnung der Ergebnisse
+sx=-vsb*cos(alpha)* (-vsb*sin(alpha) + sqrt(vsb^2*sin(alpha)^2+2*g*h))/g
