f64d810fc30c2360eae5826f076b373f9017c1a9
daniw <dani.winz@bluewin.ch>
2013-04-27 14:53:14 +0200

    Aufgabe 3.2 a

---
+++ practice/ex32.m
@@ -0,0 +1,22 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+mw=15;
+mk=23;
+mb=0.8;
+
+vb=15.9;
+
+% a
+
+% Zwischenewerte berechnen
+
+% Berechnung der Ergebnisse
+v1=(2*vb) / (1+(mk+mw)/(mb))
+
+% b
+
+% Zwischenwerte berechnen
+
+% Berechnung der Ergebnisse
