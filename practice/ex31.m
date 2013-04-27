64149a25bb1833859ffe77f1abe4c2f8c331f25e
daniw <dani.winz@bluewin.ch>
2013-04-27 13:24:51 +0200

    Aufgabe 3.1

--- practice/ex31.m
+++ practice/ex31.m
@@ -12,7 +12,9 @@ v=0.5;
 
 beta=50;
 
-% Zwischenvariabeln berechnen
+% a
+
+% Zwischenewerte berechnen
 v1x=-v1;
 v1y=0;
 
@@ -22,3 +24,14 @@ v2y=-v2*sin(beta/180*pi);
 % Berechnung der Ergebnisse
 v3x=(v*(m1+m2+m3)-v1x*m1-v2x*m2) / (m3)
 v3y=(-v1y*m1-v2y*m2) / (m3)
+
+
+% b
+
+% Zwischenwerte berechnen
+v3=sqrt(v3x^2+v3y^2);
+ekin1=(m1*v1^2 + m2*v2^2+m3*v3^2)/2;
+ekin2=(m1+m2+m3)*v^2/2;
+
+% Berechnung der Ergebnisse
+ekindiff=ekin1-ekin2
