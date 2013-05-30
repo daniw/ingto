dfc1015567af3dc174f1fbdae2c27f46321bd223
daniw <dani.winz@bluewin.ch>
2013-05-30 17:54:52 +0200

    Aufgabe 3

---
+++ practice/ex43.m
@@ -0,0 +1,30 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+i=4.6e-5;
+a=95.9;
+b=95.9;
+c=1.18;
+
+% a
+
+% Berechnung der Ergebnisse
+%~ fprintf('rad*s^2')
+%~ fprintf('s')
+
+% b
+
+% Berechnung der Ergebnisse
+a-(b/((c)^3))
+a-0
+
+% c
+
+% Berechnung der Ergebnisse
+l=i*(a-(b/((1+c)^3)))
+
+% d
+
+% Berechnung der Ergebnisse
+m=i*3*b/((1+c)^4)
