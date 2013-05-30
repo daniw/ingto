c1331fc762ca678ed7c6ebfdf1f89c28ba8e8bbe
daniw <dani.winz@bluewin.ch>
2013-05-30 17:54:34 +0200

    Aufgabe 2

---
+++ practice/ex42.m
@@ -0,0 +1,19 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+m=0.1;
+r=0.014;
+h=0.28;
+g=9.81;
+
+% a
+
+% Berechnung der Ergebnisse
+v=sqrt(10/7*g*h)
+
+% b
+
+% Berechnung der Ergebnisse
+h2=1/2*v^2/g
+
