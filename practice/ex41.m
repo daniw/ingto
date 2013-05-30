ce97c2356b6ab38adc07fd253e293a950b62f61a
daniw <dani.winz@bluewin.ch>
2013-05-30 17:02:50 +0200

    Aufgabe 1

---
+++ practice/ex41.m
@@ -0,0 +1,27 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+l=2.03;
+m=0.56;
+g=9.81;
+
+% a
+
+% Berechnung der Ergebnisse
+h=3/4*l
+
+% b
+
+% Berechnung der Ergebnisse
+ip=4/3*m*l^2
+
+% c
+
+% Berechnung der Ergebnisse
+alpha=9/8*g/l
+
+% d
+
+% Berechnung der Ergebnisse
+v=3/2*sqrt(l*g)
