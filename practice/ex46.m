a51b27510de4b3c82dae25cb9a969a0aab8c44c6
daniw <dani.winz@bluewin.ch>
2013-06-02 16:30:35 +0200

    Aufgabe 6 mit Fehler in Teil b

---
+++ practice/ex46.m
@@ -0,0 +1,21 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+m=0.067;
+M=6.52;
+h=1.96;
+b=0.8;
+v=166.4;
+d=1.02;
+
+% a
+
+% Berechnung der Ergebnisse
+omega=m*d*v/((m*d^2)+(1/3*M*h^2))
+
+% b
+
+% Berechnung der Ergebnisse
+phi=asin(1-((1/2*((m*d^2)+(1/3*M*h^2))*omega^2)/((h*M/2)+(d*m))));
+phigrad=phi/pi*180
