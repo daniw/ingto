7d16b75e5ba471c4dc896c69e8f7d6c597e907f2
daniw <dani.winz@bluewin.ch>
2013-06-02 16:39:58 +0200

    Aufgabe 7

---
+++ practice/ex47.m
@@ -0,0 +1,26 @@
+% coding:utf-8
+clear; clc;
+
+% Gegebene Variablen: 
+dreh1=0.22;
+dreh3=0.22;
+t1=0.11;
+t2=0.52;
+t3=0.11;
+
+
+% a
+
+% Berechnung der Ergebnisse
+% Es wirkt kein äusseres Drehmoment x
+
+% b
+
+% Berechnung der Ergebnisse
+f15=(1.5-dreh1-dreh3)/t2
+f25=(2.5-dreh1-dreh3)/t2
+
+% b
+
+% Berechnung der Ergebnisse
+factor=f25/(dreh1/t1)
